7be8cea951b0a55c420cc3da358e4565cc7a13dd
John Hansen <aterxerxes@gmail.com>
2022-11-22 06:54:47 -0700

    Added a title.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { RecoilRoot } from 'recoil'
+import Head from 'next/head'
 
 import PostList from "../components/PostList"
 
 export default () => ( 
   <main>
     <RecoilRoot>
+      <Head>
+        <title>John Hansen - Work Sample</title>
+      </Head>
       <PostList />
     </RecoilRoot>
   </main>
